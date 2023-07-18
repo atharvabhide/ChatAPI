0c52c011c596f82be1197043f5817980393e1777
atharvabhide <atharvabhide91@gmail.com>
2023-07-19 00:27:20 +0530

    add generic home view

--- ChatAPI/urls.py
+++ ChatAPI/urls.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+
+class ChatAPIView(GenericAPIView):
+    permission_classes = []
+    authentication_classes = []
+    def get(self, request, *args, **kwargs):
+        return Response({
+            'conversations': 'http://localhost:8000/conversations/',
+            'users': 'http://localhost:8000/users/',
+            'login': 'http://localhost:8000/auth/login/',
+            'logout': 'http://localhost:8000/auth/logout/',
+            'register': 'http://localhost:8000/auth/registration/',
+            'change_password': 'http://localhost:8000/auth/password/change/',
+            'reset_password': 'http://localhost:8000/auth/password/reset/',
+            'reset_password_confirm': 'http://localhost:8000/auth/password/reset/confirm/',
+            'reset_password_complete': 'http://localhost:8000/auth/password/reset/complete/',
+            'reset_password_confirm': 'http://localhost:8000/auth/password/reset/confirm/',
+        })
 
 urlpatterns = [
+    path('', ChatAPIView.as_view()),
     path('admin/', admin.site.urls),
     path('auth/', include('dj_rest_auth.urls')),
     path('auth/registration/', include('dj_rest_auth.registration.urls')),
